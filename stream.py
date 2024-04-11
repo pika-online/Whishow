605a7f69afdb216d62649b7c3af965e4ba4e75a1
coolEphemeroptera <605686962@qq.com>
2024-04-11 22:40:03 +0800

    uu

--- stream.py
+++ stream.py
@@ -5,39 +5,28 @@
 ############################################
 import av
 from utils import * 
-from buffer import * 
 import cv2 
 import soxr 
-
-AUDIO_FPS = 16000
-VIDEO_FPS = 30
-VIDEO_FRAME_SIZE= [640,320]
-START_STREAM = False
-
-def check_stream():
-    global START_STREAM
-    return START_STREAM
+from collections import deque
+import queue
 
 class STREAM():
     def __init__(self) -> None:
         self.pcolor = "yellow"
-        self.lock = threading.Lock()
 
     def P(self,text):
         printc("%s: %s"%(self.__class__.__name__,text),self.pcolor)
 
-    def init_state(self,cache_size):
+    def init_state(self,url,cache_size):
         self.running = True
-        self.audio_dst_fps = AUDIO_FPS
-        self.video_dst_fps = VIDEO_FPS
-        self.audio_cache_size = cache_size*self.audio_dst_fps
-        self.video_cache_size = cache_size*self.video_dst_fps
-        self.video_dst_frame_size = [640,320]
-        self.at = self.wt = -1
-        self.start_record = False
-        init_audio_cache(self.audio_cache_size)
-        init_video_cache(self.video_cache_size)
-        self.P("Finished to init the cache ..")
+        self.start_start = False
+        self.stream_end = False
+        self.AUDIO_FPS = 16000
+        self.VIDEO_FPS = 30
+        self.init_container(url)
+        self.QA = queue.Queue(cache_size*self.AUDIO_FPS)
+        self.QV = queue.Queue(cache_size*self.VIDEO_FPS)
+        self.P("Finished to init the state ..")
 
     def init_container(self,url):
         global VIDEO_FPS,START_STREAM
@@ -74,94 +63,95 @@ class STREAM():
         START_STREAM = True
         self.P("Finished to init the container ..")
 
-    def terminate(self):
-        with self.lock:
-            return self.running
-        
-    def modify_video_size(self,video_src_frame_size):
-        def finetune(size):
-            return [min(size[0],self.video_dst_frame_size[0]),min(size[1],self.video_dst_frame_size[1])]
+    
 
-        video_dst_frame_size = video_src_frame_size.copy()
-        f = [video_src_frame_size[0]/self.video_dst_frame_size[0],video_src_frame_size[1]/self.video_dst_frame_size[1]]
+    def modify_video_size(self,video_src_frame_size,video_dst_frame_size):
+        def finetune(size):
+            return [min(size[0],video_dst_frame_size[0]),min(size[1],video_dst_frame_size[1])]
+        video_mdf_frame_size = video_src_frame_size.copy()
+        f = [video_src_frame_size[0]/video_dst_frame_size[0],video_src_frame_size[1]/video_dst_frame_size[1]]
         if f[0]<1 and f[1]<1:
             id = np.argmax(f)
             k = 1/f[id]
-            video_dst_frame_size = [int(video_src_frame_size[0]*k),int(video_src_frame_size[1]*k)]
-            video_dst_frame_size = finetune(video_dst_frame_size)
-        
+            video_mdf_frame_size = [int(video_src_frame_size[0]*k),int(video_src_frame_size[1]*k)]
+            video_mdf_frame_size = finetune(video_mdf_frame_size)
         if f[0]>1 or f[1]>1:
             id = np.argmax(f)
             k = 1/f[id]
-            video_dst_frame_size = [int(video_src_frame_size[0]*k),int(video_src_frame_size[1]*k)]
-            video_dst_frame_size = finetune(video_dst_frame_size)
-
+            video_mdf_frame_size = [int(video_src_frame_size[0]*k),int(video_src_frame_size[1]*k)]
+            video_mdf_frame_size = finetune(video_mdf_frame_size)
         self.P(f"the the video src size is {video_src_frame_size}")
-        self.P(f"the the video dst size is:{video_dst_frame_size} under the target size:{self.video_dst_frame_size}")
-        return video_dst_frame_size
+        self.P(f"the the video dst size is:{video_mdf_frame_size} under the target size:{video_dst_frame_size}")
+        return video_mdf_frame_size
 
 
-    def read(self,url,video_dst_frame_size=[-1,-1],cache_size=10*60):
+    def read(self,video_dst_frame_size=[-1,-1]):
 
-        self.init_container(url)
-        self.init_state(cache_size)
-
-        if video_dst_frame_size != [-1,-1]:self.video_dst_frame_size = video_dst_frame_size
         video_src_frame_size = [self.info_video['video_width'],self.info_video['video_height']]
-        video_frame_size = self.modify_video_size(video_src_frame_size)
+        if video_dst_frame_size != [-1,-1]:
+            video_mdf_frame_size = self.modify_video_size(video_src_frame_size,video_dst_frame_size)
+        else:
+            video_mdf_frame_size = self.modify_video_size(video_src_frame_size,[640,320])
 
         # # 将视频和音频流写入输出文件
         s = time.time()
         video_count = 0
         audio_count = 0
         pcount = 0
+        at = 0
+        wt = 0
         for packet in self.container.demux():
 
             if not self.running:return
 
             if packet.stream.type == 'audio':
                 for frame in packet.decode():
-                    # print("A:%.2f"%frame.time)
-                    self.at = frame.time
-                    if self.start_record:
+                    at = frame.time
+                    if self.start_start:
                         frame = frame.to_ndarray()[0]
-                        frame = soxr.resample(frame,in_rate=self.info_audio['audio_sample_rate'],out_rate=self.audio_dst_fps)
+                        frame = soxr.resample(frame,in_rate=self.info_audio['audio_sample_rate'],out_rate=self.AUDIO_FPS)
                         frame = audio_f2i(frame,16)
-                        push_audio_cache(frame)
+                        for p in frame:
+                            self.QA.put(p)
+                        # self.P(self.QA.qsize())
                         audio_count += 1       
 
             if packet.stream.type == 'video':
                 for frame in packet.decode():
-                    # print("V:%.2f"%frame.time)
-                    self.vt = frame.time
-                    if self.start_record:
+                    wt = frame.time
+                    if self.start_start:
                         frame = frame.to_image()
                         frame = np.asarray(frame)
                         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                        frame = cv2.resize(frame,video_frame_size,interpolation=cv2.INTER_CUBIC)
+                        frame = cv2.resize(frame,video_mdf_frame_size)
                         frame= cv2.imencode('.jpg', frame,[cv2.IMWRITE_JPEG_QUALITY, 90])[1]
-                        push_video_cache(frame)
+                        self.QV.put(frame)
+                        # self.P(self.QV.qsize())
                         video_count += 1
             pcount += 1
 
-            if self.at>0 :
-                self.start_record = True
+            if at>0 :
+                self.start_start = True
 
             if(time.time()-s>1):
-                self.P("Aduio(FPS): %sK"%audio_count)
-                self.P("Video(FPS): %s"%video_count)
+                self.P("SEEK(AUDIO):%s, SEEK(VIDEO):%s"%(format_timestamp(at),format_timestamp(wt)))
+                self.P("Aduio(FPS):%sK, QSize:%s"%(audio_count,self.QA.qsize()))
+                self.P("Video(FPS):%s, QSize:%s"%(video_count,self.QV.qsize()))
                 s = time.time()
                 audio_count = 0
                 video_count = 0
                 print()
+
+        self.stream_end = True
 
         # 关闭容器和输出文件
         self.container.close()
 
 if __name__ == "__main__":
 
+    # url = "rtmp://mobliestream.c3tv.com:554/live/goodtv.sdp"
+    url = "test.mp4"
     stm = STREAM()
-    stm.init_state(2*60)
-    # stm.read("rtmp://mobliestream.c3tv.com:554/live/goodtv.sdp")
-    stm.read("test.mp4",video_dst_frame_size=[400,400],cache_size=10*60)
+    stm.init_state(url=url,cache_size=10*60)
+    stm.read(video_dst_frame_size=[-1,-1])
     pass 
--- whishow.py
+++ whishow.py
@@ -4,13 +4,11 @@
 # Where: NanJing
 ############################################
 from utils import * 
-from buffer import * 
-from buffer2 import * 
-from stream_process import *
-from stream import * 
+from sprocess import *
 import cv2 
 import pyaudio
-from PIL import Image, ImageDraw, ImageFont
+import multiprocessing as mp
+
 
 class PLAY():
     def __init__(self) -> None:
@@ -24,39 +22,39 @@ class PLAY():
     def P(self,text):
         printc("%s: %s"%(self.__class__.__name__,text),self.pcolor)
 
-    def init_state(self,start=0,step=1):
+    def init_state(self,start=0,step=1,audio_fps=16000,video_fps=30):
         self.running = True
-        self.seek_a = int(start * AUDIO_FPS)
-        self.seek_v = int(start * VIDEO_FPS)
-        self.step_a = int(step * AUDIO_FPS)
-        self.step_v = int(step * VIDEO_FPS)
+        self.AUDIO_FPS = audio_fps
+        self.VIDEO_FPS = video_fps
+
+        self.seek_a = int(start * self.AUDIO_FPS)
+        self.seek_v = int(start * self.VIDEO_FPS)
+        self.step_a = int(step * self.AUDIO_FPS)
+        self.step_v = int(step * self.VIDEO_FPS)
         self.init_audio_driver()
         self.init_video_driver()
         self.fid = 0
         self.aid = 0
         self.vid = 0
 
-
     def init_audio_driver(self):
         self.audio_driver = pyaudio.PyAudio()
         self.audio_stream =self.audio_driver.open(format=pyaudio.paInt16,
                                                     channels=1,
-                                                    rate=AUDIO_FPS,
+                                                    rate=self.AUDIO_FPS,
                                                     output=True,
                                                     )
         
     def init_video_driver(self):
         self.win_name = "Whishow Player"
         self.frame_wait = 20
-        pass
-
 
     def listen_audio(self):
         while self.running:
             with self.alock:
-                if self.audio_data:
+                if len(self.audio_data):
                     s = time.time()
-                    self.audio_stream.write(self.audio_data)
+                    self.audio_stream.write(self.audio_data.tobytes())
                     self.P(f"listen_audio -> frame[{self.aid}] -> cost:{time.time()-s}")
                     self.audio_data = []
                     self.aid += 1
@@ -66,11 +64,12 @@ class PLAY():
     def listen_video(self):
         while self.running:
             with self.vlock:
-                if self.video_data:
+                if len(self.video_data):
                     s = time.time()
                     for frame in self.video_data:
                         cv2.imshow(self.win_name, frame)
                         key = cv2.waitKey(self.frame_wait)
+                        
                     self.P(f"listen_video -> frame[{self.vid}] -> cost:{time.time()-s}")
                     self.video_data = []
                     self.vid += 1
@@ -78,79 +77,69 @@ class PLAY():
 
     
 
-    def run(self):
+    def run(self,spc:SPROCESS):
+        ps = threading.Thread(target=spc.run,args=())
         pa = threading.Thread(target=self.listen_audio,args=())
         pv = threading.Thread(target=self.listen_video,args=())
+        ps.start()
         pa.start()
         pv.start()
-        while self.running:
-            if self.seek_a+self.step_a<=get_audio2_latest() and \
-                self.seek_v+self.step_v<=get_video2_latest() and\
-                self.fid==self.aid==self.vid:
-                chunk_a = read_audio2_cache(self.seek_a,self.seek_a+self.step_a)
-                chunk_v = read_video2_cache(self.seek_v,self.seek_v+self.step_v)
-                chunk_a = np.array(chunk_a,dtype="int16").tobytes()
+        while 1:
+            if not self.running:
+                spc.running = False
+                break
+
+            if spc.QA.qsize()>=self.step_a and spc.QV.qsize()>=self.step_v and self.fid==self.aid==self.vid:
+            #    self.fid==self.aid==self.vid:
+                # 播放
+                chunk_a = [spc.QA.get() for _ in range(self.step_a)]
+                chunk_v = [spc.QV.get() for _ in range(self.step_v)]
+                chunk_a = np.array(chunk_a,dtype="int16")
+                chunk_v = np.array(chunk_v,dtype="uint8")
+
                 self.audio_data = chunk_a
                 self.video_data = chunk_v
                 self.P("Send the %s block data .."%self.fid)
+
                 self.seek_a+=self.step_a
                 self.seek_v+=self.step_v
                 self.fid += 1
-            time.sleep(0.001)
+                self.P("seek(play):%s "%(format_timestamp(self.seek_v/self.VIDEO_FPS)))
+            time.sleep(0.01)
         self.P("========== PLAY END ==================")
         
 
 
 
+
 if __name__ == "__main__":
 
-    stm = STREAM()
-    spc = SPROCESS()
-    ply = PLAY()
-    # url = sys.argv[1]
-    url = "test.mp4"
     url = "rtmp://mobliestream.c3tv.com:554/live/goodtv.sdp"
+    url = "test.mp4"
+    url = sys.argv[1]
+    
+    spc = SPROCESS()
+    spc.init_state(url=url,cache_size=10*60,step=1)
+    ply = PLAY()
+    ply.init_state(step=1,audio_fps=spc.AUDIO_FPS,video_fps=spc.VIDEO_FPS)
 
-    # 线程1：esc退出播放
+    # esc退出播放
     def engine():
         global ply
         import keyboard
         while 1:
             if keyboard.is_pressed('esc'):
+                print("exit ..")
                 break
-            time.sleep(0.01)
-        stm.running = False
+            time.sleep(0.1)
         spc.running = False
         ply.running = False
 
-    # 线程2：读取视频流和音频流 （保存一级cache）
-    def process1():
-        global stm
-        stm.read(url = url,
-                video_dst_frame_size=[-1,-1],
-                cache_size=10*60)
-
-    # 线程2：处理帧（保存二级cache）
-    def process2():
-        global spc
-        while not check_stream():time.sleep(1)
-        spc.run(cache_size=2*60,asr=False,step=1)
-
-    # 播放视频 （播放二级cache）
-    def process3():
-        global ply
-        while not check_stream():time.sleep(1)
-        ply.init_state(start=0,step=1)
-        ply.run()
-
-    
+    def play():
+        global ply,spc
+        ply.run(spc)
 
     p0 = threading.Thread(target=engine,args=())
-    p1 = threading.Thread(target=process1,args=())
-    p2 = threading.Thread(target=process2,args=())
-    p3 = threading.Thread(target=process3,args=())
-
+    p1 = threading.Thread(target=play,args=())
     p0.start()
     p1.start()
-    p2.start()
-    p3.start()
